e354921c406375705f61113d89a4c0dca7369807
Dom58 <you@example.com>
2019-05-03 15:19:34 +0000

    [ch-#165790125] Create app server and test it using ES5

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require ('body-parser');
+
+var app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req,res) =>{
+	res.send({ status:200, message:'Welcome to Quick credit web application'});
+});
+app.listen(port, () =>{
+	console.log(`Server is runnig on (http://127.0.0.1:${port})`);
+})
