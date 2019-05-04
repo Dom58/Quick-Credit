a75edad8f89b97f766084f9feebc8ad7f310dd96
Dom58 <you@example.com>
2019-05-04 15:11:09 +0000

    [ch-165807178] Modify the verify user response and edit its endpoint

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -8,6 +8,6 @@ const route = express.Router();
 route.post('/api/v1/signup', userController.signup);
 route.get('/api/v1/users',auth, userController.allUsers);
 route.post('/api/v1/signin', userController.signin);
-route.patch('/api/v1/verifyUser/:email',auth, userController.verifyUser);
+route.patch('/api/v1/users/:email/verify',auth, userController.verifyUser);
 
 export default route;
