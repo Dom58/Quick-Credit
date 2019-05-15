5a207fb8e5182c7651a96c77aa7034e485631585
Dom58 <you@example.com>
2019-05-15 22:39:36 +0000

    Modify user validator logics

--- server/helpers/userHelper.js
+++ server/helpers/userHelper.js
@@ -3,13 +3,13 @@ import joi from 'joi';
 exports.validateSignup = (user) => {
   const schema = joi.object().keys({
     // eslint-disable-next-line newline-per-chained-call
-    firstName: joi.string().min(3).max(40).required().label('First Name'),
+    firstName: joi.string().min(3).max(40).required().label('First Name').empty(/\s+/).trim(),
     lastName: joi.string().min(3).max(40).required()
-      .label('Last Name'),
+      .label('Last Name').empty(/\s+/).trim(),
     email: joi.string().email({ minDomainAtoms: 2 }).required().label('Email'),
     address: joi.string().label('User Adress'),
     isAdmin: joi.boolean().label('Admin'),
-    password: joi.string().regex(/^[a-zA-Z0-9]{6,30}$/).label('Password'),
+    password: joi.string().regex(/^[a-zA-Z0-9]{6,30}$/).label('Password').empty(/\s+/).trim(),
   });
   return joi.validate(user, schema, { abortEarly: false });
 };
@@ -17,7 +17,7 @@ exports.validateSignup = (user) => {
 exports.validateLogin = (user) => {
   const login = {
     email: joi.string().email().required().label('Email'),
-    password: joi.string().required().label('Password'),
+    password: joi.string().required().label("Password is Required").empty(/\s+/).trim(),
   };
   return joi.validate(user, login, { abortEarly: false });
 };
