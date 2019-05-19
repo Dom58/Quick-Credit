2dae5e820cdcfdfbc0314209d3b21c4b5bf2d3ca
Dom58 <you@example.com>
2019-05-19 20:37:36 +0000

    [ft-#166108182] Finishing of creation view specific loan application

--- server/controllers/LoanController.js
+++ server/controllers/LoanController.js
@@ -94,6 +94,21 @@ const loanController = {
       statusMessageFunction(res, 400, `${loanStatus.badRequestMessage}` )
     }
   },
+  async specificLoan (req, res){
+    if (req.user.isadmin === true) {
+      const { id } = req.params;
+      const findLoan = await pool.query(queryTable.fetchOneLoan,[id]);
+      if (!findLoan.rows[0]) {
+        return res.status(404).json({ status: 404, error:  `Loan with ## ${id} ## not Found! ` });
+      }
+      return res.status(200).send({
+        status: 200,
+        data: findLoan.rows[0],       
+      });
+    } else {
+      statusMessageFunction(res, 400, `${loanStatus.badRequestMessage}` )
+      }
+  },
   
 };
 export default loanController;
--- server/routes/loanRoute.js
+++ server/routes/loanRoute.js
@@ -6,7 +6,7 @@ const route = express.Router();
 
 route.post('/api/v2/loans', auth, loanController.applyLoan);
 route.get('/api/v2/loans', auth, loanController.allLoans);
-// route.get('/api/v2/loans/:id', auth, loanController.specificLoan);
+route.get('/api/v2/loans/:id', auth, loanController.specificLoan);
 // route.patch('/api/v2/loans/:id', auth, loanController.approveLoan);
 // route.post('/api/v2/loans/:id/repayment', auth, loanController.repayLoan);
 // route.get('/api/v2/loans/:id/repayments', auth, loanController.specificLoanRepayment);
