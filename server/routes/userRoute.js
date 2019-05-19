30185d3c975922edf662630ad743bb41637423c4
Dom58 <you@example.com>
2019-05-19 02:35:34 +0000

    [#166105591] Create a signup endpoint connected to postgres

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -5,9 +5,9 @@ import auth from '../middleware/auth'
 
 const route = express.Router();
 
-route.post('/api/v1/auth/signup', userController.signup);
-route.get('/api/v1/auth/users',auth, userController.allUsers);
-route.post('/api/v1/auth/signin', userController.signin);
-route.patch('/api/v1/users/:email/verify',auth, userController.verifyUser);
+route.post('/api/v2/auth/signup', userController.signup);
+// route.get('/api/v1/auth/users',auth, userController.allUsers);
+// route.post('/api/v1/auth/signin', userController.signin);
+// route.patch('/api/v1/users/:email/verify',auth, userController.verifyUser);
 
 export default route;
