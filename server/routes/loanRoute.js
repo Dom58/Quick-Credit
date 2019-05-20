d6e839cebc7b60a3753316d83db8fa72150d1fe6
Dom58 <you@example.com>
2019-05-20 10:54:06 +0000

    [ft-#166114191] Finishes to create a route to help to view all loan repayment version2

--- server/routes/loanRoute.js
+++ server/routes/loanRoute.js
@@ -10,5 +10,5 @@ route.get('/api/v2/loans/:id', auth, loanController.specificLoan);
 route.patch('/api/v2/loans/:id', auth, loanController.approveLoan);
 route.post('/api/v2/loans/:id/repayment', auth, loanController.repayLoan);
 // route.get('/api/v2/loans/:id/repayments', auth, loanController.specificLoanRepayment);
-// route.get('/api/v2/repayments/loans', auth, loanController.allRepaymentLoan);
+route.get('/api/v2/repayments/loans', auth, loanController.allRepaymentLoan);
 export default route;
