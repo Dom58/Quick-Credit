541100aebae3bda8ab40d26a968939e051966861
Dom58 <you@example.com>
2019-05-22 12:20:15 +0000

    Modify the database connection string due to the different environment

--- server/models/dbCon.js
+++ server/models/dbCon.js
@@ -3,10 +3,24 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+let pool = {}
 
- const pool = new Pool({
+if(process.env.NODE_ENV == 'test'){
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL_TEST,
+})
+// console.log("here",process.env.DATABASE_URL_TEST)
+}
+else if (process.env.NODE_ENV == 'prod') {
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL_PRO,
+})
+}
+else {
+  pool = new Pool({
     connectionString: process.env.DATABASE_URL,
   })
+}
 pool.on('connect', () => console.log('Connected to database...'));
 
 export default pool
